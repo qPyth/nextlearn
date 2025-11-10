6ac2cc5f9d23a7914c348956615b0fa53c585fe5
adil <iskakov921@gmail.com>
2025-11-11 02:01:38 +0500

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+        <div className={styles.shape} />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
@@ -25,7 +28,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+            <h1 className="text-blue-500">I'm blue!</h1>
         </div>
       </div>
     </main>
